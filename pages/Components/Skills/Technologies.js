e0d9808ec54f0f4367cf6c791cfbcb4b33c6abaa
Devender <devendersingh385@gmail.com>
2023-05-19 18:40:05 +0530

    update changes.

--- pages/Components/Skills/Technologies.js
+++ pages/Components/Skills/Technologies.js
@@ -7,6 +7,7 @@ import MoreVertIcon from '@mui/icons-material/MoreVert';
 import { red } from '@mui/material/colors';
 import { Box, Card } from '@mui/material';
 import Grid from '@mui/material/Grid';
+import HtmlIcon from '@mui/icons-material/Html';
 
 const Technologies = () => {
 
@@ -14,22 +15,82 @@ const technologies = [
      {
        id: 1,
         imagePath: '',
-        title: 'saasasasas'
+        title: 'HTML'
     },
      {
        id: 2,
         imagePath: '',
-        title: 'abc'
+        title: 'CSS'
     },
      {
        id: 3,
-        imagePath: '',
-        title: 'abc'
+        imagePath: '<svg width="100%" height="100%" viewBox="-10.5 -9.45 21 18.9" fill="none" xmlns="http://www.w3.org/2000/svg" class="mt-4 mb-3 text-link dark:text-link-dark w-24 lg:w-28 self-center text-sm mr-0 flex origin-center transition-all ease-in-out"><circle cx="0" cy="0" r="2" fill="currentColor"></circle><g stroke="currentColor" stroke-width="1" fill="none"><ellipse rx="10" ry="4.5"></ellipse><ellipse rx="10" ry="4.5" transform="rotate(60)"></ellipse><ellipse rx="10" ry="4.5" transform="rotate(120)"></ellipse></g></svg>',
+        title: 'JavaScript'
     },
      {
        id: 4,
         imagePath: '',
-        title: 'abc'
+        title: 'React'
+    },
+     {
+       id: 5,
+        imagePath: '',
+        title: 'Bootstrap'
+    },
+     {
+       id: 6,
+        imagePath: '',
+        title: 'NextJs'
+    },
+     {
+       id: 7,
+        imagePath: '',
+        title: 'Bootstrap'
+    },
+     {
+       id: 8,
+        imagePath: '',
+        title: 'Github'
+    },
+     {
+       id: 9,
+        imagePath: '',
+        title: 'Clickup'
+    },
+     {
+       id: 10,
+        imagePath: '',
+        title: 'Laravel'
+    },
+     {
+       id: 11,
+        imagePath: '',
+        title: 'Photoshop'
+    },
+     {
+       id: 12,
+        imagePath: '',
+        title: 'Illustrator'
+    },
+     {
+       id: 13,
+        imagePath: '',
+        title: 'Abobe XD'
+    },
+     {
+       id: 14,
+        imagePath: '',
+        title: 'Figma'
+    },
+     {
+       id: 15,
+        imagePath: '',
+        title: 'WordPress'
+    },
+     {
+       id: 15,
+        imagePath: '',
+        title: 'Tailwind'
     },
 ]
 
@@ -39,11 +100,15 @@ const technologies = [
             technologies.map( technology => 
                 // eslint-disable-next-line react/jsx-key
                 <Grid item xs={4}  display="flex" alignItems="center">
-                    <Box key={technology.id}>                    
+                    <Box key={technology.id} sx={{
+                        width: '100%'
+                    }}>                    
                             <CardHeader 
+                                    className='border w-full rounded-lg'
                                 avatar={
                                 <Avatar sx={{ bgcolor: red[500] }} aria-label="recipe">
-                                    
+                                    <HtmlIcon />
+                                   
                                 </Avatar>
                                 }
                                 action={
